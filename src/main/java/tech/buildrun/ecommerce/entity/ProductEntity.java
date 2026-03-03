a0194052ee4ccc7552a76ab4ee1a443893be9a9d
Augusto fragg <augustofragg@gmail.com>
2026-03-03 16:26:54 -0300

    Performing @OneToMany and @ManyToOne mapping (Order, OrderItem, and Product)

---
+++ src/main/java/tech/buildrun/ecommerce/entity/ProductEntity.java
@@ -0,0 +1,50 @@
+package tech.buildrun.ecommerce.entity;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_products")
+public class ProductEntity {
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pruductsId;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal productPrice;
+
+
+    public ProductEntity() {
+    }
+
+    public Long getPruductsId() {
+        return pruductsId;
+    }
+
+    public void setPruductsId(Long pruductsId) {
+        this.pruductsId = pruductsId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(BigDecimal productPrice) {
+        this.productPrice = productPrice;
+    }
+}
